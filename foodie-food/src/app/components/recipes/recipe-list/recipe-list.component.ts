5a8df276200538358861ab4f769c5332f0258f6c
Vihanga Malinda Fernando <vihangamalindafernando@gmail.com>
2022-12-26 01:24:19 +0530

    Adding Content to the Recipies Component.

--- foodie-food/src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ foodie-food/src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class RecipeListComponent {
 
+  recipes:Recipe[] = [
+    new Recipe('Test Recipe','Simple Test','https://vismaifood.com/storage/app/uploads/public/7b2/49e/d12/thumb__1200_0_0_0_auto.jpg')
+  ]
 }
