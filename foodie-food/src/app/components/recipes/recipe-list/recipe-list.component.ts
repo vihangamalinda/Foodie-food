cad632bfb90a985be1143ff5832108c8dd838d24
Vihanga Malinda Fernando <vihangamalindafernando@gmail.com>
2023-01-22 13:30:03 +0530

    Pasing Data with event and property binding.

--- foodie-food/src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ foodie-food/src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component ,Output,EventEmitter} from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,10 +7,17 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  @Output("selectedRecipeItem") wasSelectedRecipe = new EventEmitter<Recipe>();
 
   recipes:Recipe[] = [
-    new Recipe('Test Recipe','Simple Test','https://vismaifood.com/storage/app/uploads/public/7b2/49e/d12/thumb__1200_0_0_0_auto.jpg'),
-    new Recipe('Test Recipe','Simple Test','https://vismaifood.com/storage/app/uploads/public/7b2/49e/d12/thumb__1200_0_0_0_auto.jpg'),
-    new Recipe('Test Recipe','Simple Test','https://vismaifood.com/storage/app/uploads/public/7b2/49e/d12/thumb__1200_0_0_0_auto.jpg'),
-  ]
+    new Recipe('Test Recipe 01','Simple Test','https://vismaifood.com/storage/app/uploads/public/7b2/49e/d12/thumb__1200_0_0_0_auto.jpg'),
+    new Recipe('Test Recipe 02 ','Simple Test','https://vismaifood.com/storage/app/uploads/public/7b2/49e/d12/thumb__1200_0_0_0_auto.jpg'),
+    new Recipe('Test Recipe 03','Simple Test','https://vismaifood.com/storage/app/uploads/public/7b2/49e/d12/thumb__1200_0_0_0_auto.jpg'),
+  ];
+
+  onSelectedItem(item:Recipe){
+    this.wasSelectedRecipe.emit(item);
+  }
+  
+
 }
