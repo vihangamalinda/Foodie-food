077be8b2b0be3ef940a55c1e897696e30f037111
Vihanga Malinda Fernando <vihangamalindafernando@gmail.com>
2023-01-22 10:33:02 +0530

    Adding navigation with Event Bining and ngIf.

--- foodie-food/src/app/components/header/header.component.ts
+++ foodie-food/src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Output,EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  
+  @Output("feature") selectedFeature = new EventEmitter<string>();
 
+  onSelect(feature:string){
+    this.selectedFeature.emit(feature);
+  }
 }
